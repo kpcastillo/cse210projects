dc70e71895f1d3284437e33ce41eeecb6e2c3605
kpcastillo <krn_iveth09@hotmail.com>
2023-01-07 10:31:46 -0800

    finished assigment

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade? ");
+        int userGrade = int.Parse(Console.ReadLine());
+
+        string letter = "";
+
+        if (userGrade > 90 || userGrade == 90)
+        {
+            letter = "A";
+    
+        }
+        else if(userGrade > 80 || userGrade == 80)
+        {
+            letter = "B";
+        }
+         else if(userGrade > 70 || userGrade == 70)
+        {
+            letter = "C";
+        }
+         else if(userGrade > 60 || userGrade == 60)
+        {
+            letter = "D";
+        }
+         else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is {letter}");
+
+        if (userGrade > 70)
+        {
+            Console.WriteLine("Congratulations you passed!!!");
+        }
+        else
+        {
+            Console.WriteLine("You did not pass, better luck next time.");
+        }
+
+
     }
 }
