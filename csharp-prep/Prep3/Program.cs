6cad8dc33ab424c71bf379acdc36bde84e05f207
kpcastillo <krn_iveth09@hotmail.com>
2023-01-11 10:46:56 -0800

    activity3 and 4 done

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //int magicNum = int.Parse(Console.ReadLine());
+        //Console.Write("What is your guess? ");
+        //int userGuess = int.Parse(Console.ReadLine());
+
+        Random RandomGenerator = new Random();
+        int magicNum = RandomGenerator.Next(1,101);
+
+        int userGuess = -1;
+
+        while (!(userGuess == magicNum))
+
+        if (userGuess > magicNum)
+        {
+            Console.WriteLine("Guess Lower! ");
+        }
+        else if (userGuess < magicNum)
+        {
+            Console.WriteLine("Guess Higher!");
+        }
+        else 
+        {
+            Console.WriteLine("You guessed it! ");
+        }
+        
+        for (int i = userGuess; i>1; i++)
+        {
+            Console.WriteLine(i);
+        }
+        
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, Type 0 when finished.");
+
+        List<int> numbers = new List<int>();
+
+        int  userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.Write("Enter number: ");
+            userNumber = int.Parse(Console.ReadLine());
+            if (userNumber !=0)
+            {
+            numbers.Add(userNumber);
+            }
+           
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"The sum is: {sum}");
+
+        float avarage = ((float)sum)/numbers.Count;
+        Console.WriteLine($"The avarage is: {avarage}");
+
+        int maxNumber = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > maxNumber)
+            {
+                maxNumber = number;
+            }
+        }
+        Console.WriteLine($"The max number is {maxNumber}");
     }
 }
