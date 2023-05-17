efe10205bfd08a355eb9f8e38e7c5a42ba98861c
kpcastillo <krn_iveth09@hotmail.com>
2023-05-17 14:08:24 -0700

    Learning03

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+
+        Fraction firstCost = new Fraction();
+        Fraction secondConst = new Fraction(6);
+        Fraction thirdConst = new Fraction(6,7);
+
+        Console.WriteLine(firstCost.GetFractionString());
+        Console.WriteLine(firstCost.GetDecimalValue());
+
+        Console.WriteLine(secondConst.GetFractionString());
+        Console.WriteLine(secondConst.GetDecimalValue());
+
+        Console.WriteLine(thirdConst.GetFractionString());
+        Console.WriteLine(thirdConst.GetDecimalValue());
+
+
+
     }
+    
 }
