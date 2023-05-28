c20ad20354bcc78d35f12b225bc06ef42ff0ce91
kpcastillo <krn_iveth09@hotmail.com>
2023-05-27 23:09:53 -0700

    program edit

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -6,9 +6,9 @@ class Program
     {
         Console.WriteLine("Welcome to the scripture memorizer program");
 
-        Reference ref1 = new Reference();
-        Scripture scr1 = new Scripture ();
-        HideWord refAndScrip = new HideWord(ref1, scr1);
+        //Reference ref1 = new Reference();
+        //Scripture scr1 = new Scripture ();
+        HideWord refAndScrip = new HideWord();
 
         refAndScrip.HideWordFromSentece();
 
