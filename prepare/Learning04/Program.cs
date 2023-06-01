1c2fe530a0f9c0fea8c211feeec8aa2438d3fa0b
kpcastillo <krn_iveth09@hotmail.com>
2023-05-31 20:58:55 -0400

    finished learning activity

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -7,5 +7,13 @@ class Program
         Console.WriteLine("Hello Learning04 World!");
         Assigment assigment1 = new Assigment("Samuel Bannet", "Multoplication");
         Console.WriteLine(assigment1.GetSummary());
+
+        MathAssigment math1 = new MathAssigment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+        Console.WriteLine(math1.GetSummary());
+        Console.WriteLine(math1.GetHomeworkList());
+
+        WritingAssigment writ1 = new WritingAssigment("Mary Walters", "European History", "The Causes of World War II by Mary Waters");
+        Console.WriteLine(writ1.GetWritingInfo());
+
     }
 }
