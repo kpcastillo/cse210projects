fd6af71cb59d59d6488c9fdd3e1c264c1cbd1360
kpcastillo <krn_iveth09@hotmail.com>
2023-06-02 21:28:50 -0400

    prove week 8 started

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Develop04 World!");
+        //Have a menu system to allow the user to choose an activity.
     }
 }
