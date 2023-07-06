fc267d0c3345afbabfd8c3a9d2093ef542cbea3f
kpcastillo <krn_iveth09@hotmail.com>
2023-07-06 11:02:55 -0700

    project 2 started

--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -5,5 +5,7 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Foundation2 World!");
+        //two orders with a 2-3 products each. Call the methods to get the packing label, the shipping label,
+        // and the total price of the order, and display the results of these methods.
     }
 }
